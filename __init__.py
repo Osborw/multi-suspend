9ee2c8a0985e10e8ca4ac8fa5efabdff2da3ed94
Wayne Osborn <jacobwayne1513@aol.com>
2024-02-15 22:55:16 -0500

    cleanup function and rename

--- __init__.py
+++ __init__.py
@@ -1,29 +1,20 @@
-# import the main window object (mw) from aqt
 from aqt import mw
-# import the "show info" tool from utils.py
 from aqt.utils import showInfo, qconnect
-# import all of the Qt GUI library
 from aqt.qt import *
 
-# We're going to add a menu item below. First we want to create a function to
-# be called when the menu item is activated.
+def main():
+    tag = '#AK_Step1_v11::#UWorld::0-99::01'
+    unsuspendCardsByTag(tag)
 
-def testFunction() -> None:
-    # get the number of cards in the current collection, which is stored in
-    # the main window
-    ids = mw.col.find_cards("tag:#AK_Step1_v11::#UWorld::0-99::01")
-    print('match length: ', len(ids))
+def unsuspendCardsByTag(tag) -> None:
+
+    ids = mw.col.find_cards('tag:' + tag)
+
     if len(ids) == 0:
-        showInfo('sori no matches')
+        showInfo('Found no matches by tag ' + tag + '. Perhaps that tag does not exist or is typed incorrectly.')
     else:
         mw.col.sched.unsuspendCards(ids)
 
-    # show a message box
-    # showInfo("Card count: %d" % cardCount)
-
-# create a new menu item, "test"
-action = QAction("test", mw)
-# set it to call testFunction when it's clicked
-qconnect(action.triggered, testFunction)
-# and add it to the tools menu
+action = QAction("Multi-Unsuspend", mw)
+qconnect(action.triggered, unsuspendCardsByTag)
 mw.form.menuTools.addAction(action)
